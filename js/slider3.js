b3c0962a3c1c41bbc1e753101894162c787e84ee
Mustafa Ramazanov <mustafa.ramazanov.2002@gmail.com>
2023-07-28 00:13:29 +0300

    appartaments-page

---
+++ js/slider3.js
@@ -0,0 +1,31 @@
+const splide = new Splide( '.splide', {
+    type   : 'loop',
+    perPage: 1,
+    perMove: 1,
+    pagination:false,
+    type:'fade',
+  } );
+  
+  splide.mount();
+
+
+  const splide2 = new Splide( '#slider2', {
+    rewind          : true,
+    fixedWidth      : 200,
+    fixedHeight     : 100,
+    isNavigation    : true,
+    gap             : 10,
+    focus           : 'center',
+    pagination      : false,
+    cover           : true,
+    dragMinThreshold: {
+      mouse: 4,
+      touch: 10,
+    },
+   })
+   splide2.mount();
+
+
+splide.sync(splide2);
+splide.mount();
+splide2.mount();
