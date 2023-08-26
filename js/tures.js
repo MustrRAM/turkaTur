4ae94024742ac3ec584245175e3954de01a794e7
Mustafa Ramazanov <mustafa.ramazanov.2002@gmail.com>
2023-08-26 17:52:29 +0300

    scroll-animation-go

--- js/tures.js
+++ js/tures.js
@@ -14,4 +14,25 @@ const socialBox = document.querySelector('.social__box');
 socialButton.addEventListener('click', function(){
     socialBox.classList.add('active');
     socialButton.classList.add('hidden');
-})
+});
+
+
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('active');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.3] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('#animation');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
+
+  
