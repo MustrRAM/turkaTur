a9fa432acb355c0e362728b677056fc3533279fa
Mustafa Ramazanov <mustafa.ramazanov.2002@gmail.com>
2023-08-31 01:24:24 +0300

    appertaments1-2

--- js/appartaments.js
+++ js/appartaments.js
@@ -6,3 +6,22 @@ socialOpen.addEventListener('click', function(){
     socialOpen.classList.add('hidden');
 })
 
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('active');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.3] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('#animation');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
+
+  
